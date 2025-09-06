89576ab902a57d419f5abcffab273559acaac5dd
Mika Morgan <112218152+mika-morgan@users.noreply.github.com>
2025-09-06 11:25:49 -0500

    Update glide_web.py

--- glide_web.py
+++ glide_web.py
@@ -99,6 +99,7 @@ for category, commands in toolbox.items():
 
 # --- Main Editor ---
 st.subheader("Code Editor")
+code_input = st.text_area(" ", height=300, key="code_editor_text")
 
 # --- Run Code ---
 if st.button("Run Code"):
